51324fd81899060dde42e8c1835405d722c27762
starhq <50849806@qq.com>
2017-03-16 17:32:01 +0800

    有些忘了提交

---
+++ apigateway/apigatewayservice/src/main/java/com/iluwatar/api/gateway/App.java
@@ -0,0 +1,20 @@
+package com.iluwatar.api.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Hello world!
+ */
+@SpringBootApplication
+public class App {
+
+    /**
+     * Program entry point
+     *
+     * @param args command line args
+     */
+    public static void main(String[] args) {
+        SpringApplication.run(App.class, args);
+    }
+}
