98d57b90351ea725f335e162397fea8a96fde3ce
starhq <50849806@qq.com>
2018-03-08 23:16:16 +0800

    add state pattern

---
+++ state/src/main/java/com/iluwatar/state/PeacefulState.java
@@ -0,0 +1,26 @@
+package com.iluwatar.state;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class PeacefulState implements State {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(PeacefulState.class);
+
+    private Mammoth mammoth;
+
+    public PeacefulState(Mammoth mammoth) {
+        this.mammoth = mammoth;
+    }
+
+    @Override
+    public void observe() {
+        LOGGER.info("{} is calm and peaceful.", mammoth);
+    }
+
+    @Override
+    public void onEnterState() {
+        LOGGER.info("{} calms down.", mammoth);
+    }
+
+}
---
+++ state/src/main/java/com/iluwatar/state/State.java
@@ -0,0 +1,7 @@
+package com.iluwatar.state;
+
+public interface State {
+    void onEnterState();
+
+    void observe();
+}
